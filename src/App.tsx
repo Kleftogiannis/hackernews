c012f466c2149a89dc88eae988ccf9703cfd698e
kleftogiannis <kleftogiannis@oktopay.eu>
2025-02-09 00:18:57 +0200

    testing useQuery

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,14 @@
 import "./App.css";
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import SearchInput from "./components/SearchInput";
 import AutoComplete from "./components/AutoComplete";
 import SavedStories from "./components/SavedStories";
-
+import {
+  QueryClient,
+  QueryClientProvider,
+  useQuery,
+} from "@tanstack/react-query";
+const queryClient = new QueryClient();
 export interface Result {
   title: string;
   author: string;
@@ -14,7 +19,14 @@ export interface Result {
 function App() {
   const [searchTerm, setSearchTerm] = useState("");
   const [results, setResults] = useState<Result[]>([]);
-  const [savedItems, setSavedItems] = useState<Result[]>([]);
+  const [savedItems, setSavedItems] = useState<Result[]>(() => {
+    const storedItems = sessionStorage.getItem("savedItems");
+    return storedItems ? JSON.parse(storedItems) : [];
+  });
+
+  useEffect(() => {
+    sessionStorage.setItem("savedItems", JSON.stringify(savedItems));
+  }, [savedItems]);
 
   const fetchResults = async (searchTerm: string) => {
     if (searchTerm.length < 3) {
@@ -32,6 +44,15 @@ function App() {
     }
   };
 
+  // const { data: results = [] , error, isLoading } = useQuery<Result[]>({
+  //   queryKey: ["stories"],
+  //   queryFn: () => fetchResults(searchTerm),
+  //   staleTime: Infinity,
+  // });
+
+  // if (isLoading) return <h1>Loading stories...</h1>;
+  // if (error) return <h1>error during fetching stories</h1>;
+
   const handleSelect = (item: Result) => {
     setSavedItems([...savedItems, item]);
     setSearchTerm("");
@@ -43,7 +64,7 @@ function App() {
   };
 
   return (
-    <>
+    <QueryClientProvider client={queryClient}>
       <SearchInput
         searchTerm={searchTerm}
         setSearchTerm={setSearchTerm}
@@ -51,7 +72,7 @@ function App() {
       />
       <AutoComplete results={results} handleSelect={handleSelect} />
       <SavedStories savedItems={savedItems} handleDelete={handleDelete} />
-    </>
+    </QueryClientProvider>
   );
 }
 
